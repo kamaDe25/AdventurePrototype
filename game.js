a9a71d4148fc79cd94e1f4880d233c75db99cf74
kamaDe25 <kamalikade25@gmail.com>
2026-04-30 14:23:57 -0700

    trying to change first adventure scene

--- game.js
+++ game.js
@@ -1,6 +1,71 @@
+/*
 class Demo1 extends AdventureScene {
     constructor() {
         super("demo1", "First Room");
+    }
+
+    onEnter() {
+
+        let clip = this.add.text(this.w * 0.3, this.w * 0.3, "📎 paperclip")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => this.showMessage("Metal, bent."))
+            .on('pointerdown', () => {
+                this.showMessage("No touching!");
+                this.tweens.add({
+                    targets: clip,
+                    x: '+=' + this.s,
+                    repeat: 2,
+                    yoyo: true,
+                    ease: 'Sine.inOut',
+                    duration: 100
+                });
+            });
+
+        let key = this.add.text(this.w * 0.5, this.w * 0.1, "🔑 key")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => {
+                this.showMessage("It's a nice key.")
+            })
+            .on('pointerdown', () => {
+                this.showMessage("You pick up the key.");
+                this.gainItem('key');
+                this.tweens.add({
+                    targets: key,
+                    y: `-=${2 * this.s}`,
+                    alpha: { from: 1, to: 0 },
+                    duration: 500,
+                    onComplete: () => key.destroy()
+                });
+            })
+
+        let door = this.add.text(this.w * 0.1, this.w * 0.15, "🚪 locked door")
+            .setFontSize(this.s * 2)
+            .setInteractive()
+            .on('pointerover', () => {
+                if (this.hasItem("key")) {
+                    this.showMessage("You've got the key for this door.");
+                } else {
+                    this.showMessage("It's locked. Can you find a key?");
+                }
+            })
+            .on('pointerdown', () => {
+                if (this.hasItem("key")) {
+                    this.loseItem("key");
+                    this.showMessage("*squeak*");
+                    door.setText("🚪 unlocked door");
+                    this.gotoScene('demo2');
+                }
+            })
+
+    }
+}
+*/
+
+class Forest extends AdventureScene {
+     constructor() {
+        super("forest", "First Room");
     }
 
     onEnter() {
@@ -101,7 +166,7 @@ class MyIntro extends Phaser.Scene{
         this.add.text(50, 100, "Click anywhere to begin.").setFontSize(20);
         this.input.on('pointerdown', () => {
             this.cameras.main.fade(1000, 0,0,0);
-            this.time.delayedCall(1000, () => this.scene.start('demo1'));
+            this.time.delayedCall(1000, () => this.scene.start('forest'));
         });
     }
 }
@@ -125,7 +190,7 @@ const game = new Phaser.Game({
         width: 1920,
         height: 1080
     },
-    scene: [MyIntro, Demo1, Demo2, Outro],
+    scene: [MyIntro, Forest, Demo2, Outro],
     title: "Adventure Game",
 });
 
